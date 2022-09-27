7821b9f9f4afcfe125d85cc99a0a6e8a8e5b3430
Alana <halo2382001@yahoo.com>
2022-09-27 17:49:10 -0400

    added links to icons in menu

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,9 +1,17 @@
 import Image from "next/image";
 import Link from "next/link";
-import React from "react";
-import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
+import React, { useState } from "react";
+import { AiOutlineClose, AiOutlineMail, AiOutlineMenu } from "react-icons/ai";
+import { FaGithub, FaLinkedinIn } from "react-icons/fa";
+import { BsPersonLinesFill } from "react-icons/bs";
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+
+  const handleNav = () => {
+    setNav(!nav);
+  };
+
   // z-[100] will ensure this will always be on top.
   return (
     <div className="fixed w-full h-20 shadow-xl z-[100]">
@@ -37,25 +45,84 @@ const Navbar = () => {
               </li>
             </Link>
           </ul>
-          <div className="md:hidden">
+          <div onClick={handleNav} className="md:hidden">
             <AiOutlineMenu size={25} />
           </div>
         </div>
+
         {/* overlay class for mobile menu */}
-        <div className="fixed left-0 top-0 w-full h-screen bg-black/70">
+        <div
+          className={
+            nav
+              ? "md:hidden fixed left-0 top-0 w-full h-screen bg-black/70"
+              : ""
+          }>
           {/* overlay div for mobile menu */}
-          <div className="fixed left-0 top-o w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500">
+          <div
+            className={
+              nav
+                ? " fixed left-0 top-o w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500"
+                : "fixed left-[-100%] top-0 p-10 ease-in duration-500"
+            }>
             <div>
-                <div className='flex w-full items-center justify-between'>
-                    {/* pulling over image smaller and moving aioutline close to be in flex with image */}
-                    <Image src="/../public/assets/navLogo.png" width='87' height='35' alt='/' />
-                    <div className='rounded-full shadow-xl shadow-gray-400 p-3 cursor-pointer'>
-                        <AiOutlineClose />
-                    </div>
+              <div className="flex w-full items-center justify-between">
+                {/* pulling over image smaller and moving aioutline close to be in flex with image */}
+                <Image
+                  src="/../public/assets/navLogo.png"
+                  width="87"
+                  height="35"
+                  alt="/"
+                />
+                <div
+                  onClick={handleNav}
+                  className="rounded-full shadow-xl shadow-gray-400 p-3 cursor-pointer">
+                  <AiOutlineClose />
                 </div>
-                <div className='border-b border-gray-300 my-4'>
-                    <p>Things aren’t always #000000 and #FFFFFF</p>
+              </div>
+              <div className="border-b border-gray-300 my-4">
+                <p className="w=[85%] md:w-[90%] py-4">
+                  Things aren’t always #000000 and #FFFFFF
+                </p>
+              </div>
+            </div>
+            <div className="py-4 flex flex-col">
+              <ul className="uppercase">
+                <Link href="/">
+                  <li className="py-4 text-sm">Home</li>
+                </Link>
+                <Link href="/">
+                  <li className="py-4 text-sm">About</li>
+                </Link>
+                <Link href="/">
+                  <li className="py-4 text-sm">Skills</li>
+                </Link>
+                <Link href="/">
+                  <li className="py-4 text-sm">Projects</li>
+                </Link>
+                <Link href="/">
+                  <li className="py-4 text-sm">Contact</li>
+                </Link>
+              </ul>
+              <div className="pt-40 ">
+                <p className="uppercase tracking-widest text-[#5651e5]">
+                  Let's Connect!
+                </p>
+                {/* a href anchor tags only for external links, internal will cause errors and a constant refresh on pages within app */}
+                <div className="flex items-center justify-between my-4 w-full sm:w-[80%]">
+                  <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in-duration-300">
+                  <a href='https://www.linkedin.com/in/alana-mckeel-web-dev/' target="_blank" rel="noopener noreferrer"><FaLinkedinIn /></a>
+                  </div>
+                  <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in-duration-300">
+                  <a href='https://github.com/alanam79' target="_blank" rel="noopener noreferrer"><FaGithub /></a>
+                  </div>
+                  <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in-duration-300">
+                  <a href="mailto:name@gmail.com"><AiOutlineMail /></a>
+                  </div>
+                  <div className="rounded-full shadow-lg shadow-gray-400 p-3 cursor-pointer hover:scale-105 ease-in-duration-300">
+                    <BsPersonLinesFill />
+                  </div>
                 </div>
+              </div>
             </div>
           </div>
         </div>
