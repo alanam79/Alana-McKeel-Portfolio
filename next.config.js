adff353a7f3aad2c855c3d866b4bc3f89d092185
Alana <halo2382001@yahoo.com>
2022-10-10 18:37:21 -0400

    working on cache error

--- next.config.js
+++ next.config.js
@@ -2,12 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-};
-
-module.exports = {
   eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
     ignoreDuringBuilds: true,
   },
 };
