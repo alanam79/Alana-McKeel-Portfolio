b174b8fd6c740a16ac46f66f35a2323e75ea0f2f
Alana <halo2382001@yahoo.com>
2022-10-10 17:38:18 -0400

    added profile picture

--- components/About.jsx
+++ components/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
-import AboutImg from "../public/assets/laptop2.jpg";
+import AboutImg from "../public/assets/profile.jpg";
 
 const About = () => {
   return (
@@ -13,12 +13,20 @@ const About = () => {
           </p>
           <h2 className="py-4">Who I Am</h2>
           <p className="py-2 text-gray-600">Ready to Rock and Code!</p>
-          <p className="py-2 text-gray-600">Fill in Bio Later</p>
-          <p className="py-2 text-gray-600">Fill in Bio Later</p>
-          <Link href="/#projects">
-          <p className="py-2 text-gray-600 underline cursor-pointer">
-            Check out some of my latest projects.
+          <p className="py-2 text-gray-600">
+            Professionally, I have had prior experience in Business and Hotel
+            management as well as Title and Escrow experience. Currently, I am
+            focused on securing a full-time position as a Full Stack Developer.
           </p>
+          <p className="py-2 text-gray-600">
+            As I have progressed thru the course, I have been drawn to the back
+            end and have really enjoyed learning and understanding the
+            processes. Thank you for visiting, let's connect!
+          </p>
+          <Link href="/#projects">
+            <p className="py-2 text-gray-600 underline cursor-pointer">
+              Check out some of my latest projects.
+            </p>
           </Link>
         </div>
         <div className="w-full h-auto m-auto shadow-xl shadow-gray-600 rounded-xl flex items-center justify-center p-4 hover:scale-105 ease-in duration-300">
